a19f638276232321fa81e25f09874e8c521e22f7
Ricardo Ramirez <ricardo.ramirez@pnd.as>
2016-10-12 22:17:50 -0600

    Redirect to directory after log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def after_sign_in_path_for(resource)
-  	if current_entity.complete?
-  	  root_url
-  	else
-  	  edit_entity_path(current_entity)
-  	end
+  def after_sign_in_path_for(_resource)
+    if current_entity.complete?
+      entities_path
+    else
+      edit_entity_path(current_entity)
+    end
   end
 end
