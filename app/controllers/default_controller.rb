1c3315961695651d9fa47f243a7c9272d055c5a1
Ricardo M(mejiaro) <comet@Comets-MacBook-Pro.local>
2016-10-14 16:05:45 -0600

    add applications form

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -1,4 +1,8 @@
 class DefaultController < ApplicationController
   def index
   end
+
+  def apply
+  	@request = Request.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'request/create'
+
   #get 'default/index'
   root :to => 'default#index'
   devise_for :entities, :controllers => { :invitations => 'entities/invitations' }#, :path_prefix => 'devise'
   resources :entities, :path => "empresas"#, only: [:edit, :show, :update]
+  resources :requests, only: [:create]
+
+  get '/solicitar' => 'default#apply', as: 'apply'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
