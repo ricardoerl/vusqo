c43a50ddac602efbc9d15a182930f37e4904e888
Ricardo Ramirez <ricardo.ramirez@pnd.as>
2016-10-20 16:24:46 -0600

    Add social sharing script

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,4 +17,22 @@
     //   $(this).toggleClass('dn');
     // });
   }
+
+  $(".social-share").on("click", function(e) {
+    e.preventDefault();
+    windowShare($(this).attr("href"), 500, 300);
+  });
+
+  function windowShare(url, width, height) {
+    // Calculate the position of the popup so
+    // it’s centered on the screen.
+    var left = (screen.width / 2) - (width / 2),
+        top = (screen.height / 2) - (height / 2);
+
+    window.open(
+      url,
+      "",
+      "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width=" + width + ",height=" + height + ",top=" + top + ",left=" + left
+    );
+  }
 })(jQuery);
