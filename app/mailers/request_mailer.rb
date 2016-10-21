a442c6f4e0a8f2a379e53129e1a1d5b57bf4d880
Ricardo M(mejiaro) <comet@Comets-MacBook-Pro.local>
2016-10-21 17:23:17 -0600

    fix request mailer

--- app/mailers/request_mailer.rb
+++ app/mailers/request_mailer.rb
@@ -2,8 +2,8 @@ class RequestMailer < ApplicationMailer
   default from: 'no-reply@vusqo.com'
   layout 'mailer'
 
-  def new(request)
+  def new_email(request)
     @request = request
-    mail(to: 'ricardo.ramirez@gmail.com', subject: 'Nuevo registro en Vusqo')
+    mail(to: 'ricardo.ramirezlara@gmail.com', subject: 'Nuevo registro en Vusqo')
   end
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -2,6 +2,6 @@ class Request < ApplicationRecord
   after_save :send_email
 
   def send_email
-    RequestMailer.new(self).deliver
+    RequestMailer.new_email(self).deliver
   end
 end
