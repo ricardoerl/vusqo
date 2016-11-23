78ba2874c4bf687942c3563a997f671120cb4d26
Ricardo Ramirez <ricardo.ramirez@pnd.as>
2016-11-23 17:14:52 -0600

    Retunr random entities in home

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -1,6 +1,6 @@
 class DefaultController < ApplicationController
   def index
-    @entities = Entity.where('name is not null').limit(3)
+    @entities = Entity.where('name is not null').limit(3).order('RANDOM()')
   end
 
   def apply
